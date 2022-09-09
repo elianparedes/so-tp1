4ee696e718ff6f563bb15112260954e9ab32acfb
Juan Burda <jburda@itba.edu.ar>
2022-09-09 16:51:15 -0300

    feat: md5 - slave communication

--- src/slave.c
+++ src/slave.c
@@ -1,7 +1,17 @@
 #include <stdio.h>
+#include <unistd.h>
+#define BUFFER 512
+#define LOAD_FACTOR 2
 
 int main(void)
-{
-
+{       
+    char path[BUFFER*LOAD_FACTOR];
+    while (1){
+        int num_bytes= read(0, path, BUFFER*LOAD_FACTOR);
+        path[num_bytes]='\0';
+        if ( fork()==0){
+            execl("/bin/md5sum", "md5sum", path, (char *)NULL);
+        }
+    }
     return 0;
 }
