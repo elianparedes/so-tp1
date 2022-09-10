54e41845b3163d30a87f23e8ee368751c265a0a4
Elian Paredes <elianparedes.dev@gmail.com>
2022-09-10 18:47:35 -0300

    fix: remaining files not being sent to slaves

--- src/slave.c
+++ src/slave.c
@@ -2,64 +2,66 @@
 #define _POSIX_C_SOURCE
 
 #include <stdio.h>
-#include <unistd.h>
+#include <stdlib.h>
+#include <string.h>
 #include <sys/types.h>
 #include <sys/wait.h>
-#include <string.h>
-#include <stdlib.h>
+#include <unistd.h>
 
-#define BUFFER 512
+#define BUFFER     512
 
-#define MD5_CMD "md5sum "
-#define CMD_SIZE 10
-#define RD_ONLY "r"
+#define MD5_CMD    "md5sum "
+#define CMD_SIZE   10
+#define RD_ONLY    "r"
 
 #define ERROR_CODE -1
 
-char * output_buffer=NULL;
+char *output_buffer = NULL;
 
 void exit_program(int exit_status);
 
-int main(void)
-{   
+int main(void) {
 
-    while(1){
-        
+    while (1) {
         char input_buffer[BUFFER];
-        input_buffer[0]='\0';
-        size_t line_buffer = BUFFER;
         char command[BUFFER + CMD_SIZE] = MD5_CMD;
-    
-        FILE * md5_fd;
-        if (scanf("%s", input_buffer) == EOF){
+
+        FILE *md5_fd;
+        size_t line_buffer = BUFFER;
+
+        int nbytes;
+        if ((nbytes = read(0, input_buffer, BUFFER)) == -1) {
             exit_program(EXIT_FAILURE);
         }
-        else{
-            perror(input_buffer);
-        }
+
+        input_buffer[nbytes] = '\0';
 
         strcpy(command, MD5_CMD);
         strcat(command, input_buffer);
 
-        if ( (md5_fd = popen(command, RD_ONLY)) == NULL){
+        if ((md5_fd = popen(command, RD_ONLY)) == NULL) {
             perror("popen");
             exit_program(EXIT_FAILURE);
         }
-        while (getline((char **)&output_buffer, &line_buffer , md5_fd) != EOF){
+
+        while (getline((char **)&output_buffer, &line_buffer, md5_fd) != EOF) {
             printf("process pid: %d | %s", getpid(), output_buffer);
         }
+
         fflush(stdout);
 
-        if (pclose(md5_fd) == ERROR_CODE){
+        if (pclose(md5_fd) == ERROR_CODE) {
             exit_program(EXIT_FAILURE);
         }
     }
+
     exit_program(EXIT_SUCCESS);
 }
 
-void exit_program(int exit_status){
-    if (output_buffer != NULL){
+void exit_program(int exit_status) {
+    if (output_buffer != NULL) {
         free(output_buffer);
     }
+
     _exit(exit_status);
 }
