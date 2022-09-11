7f225fdcf3b8af9f02dffd0278a40d610e4bb3f2
Juan Burda <jburda@itba.edu.ar>
2022-09-11 18:45:46 -0300

    fix: slave.c optimization

--- src/slave.c
+++ src/slave.c
@@ -1,5 +1,4 @@
-#define _DEFAULT_SOURCE
-#define _POSIX_C_SOURCE
+#include "slave.h"
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -8,30 +7,22 @@
 #include <sys/wait.h>
 #include <unistd.h>
 
-#define BUFFER     512
-
-#define MD5_CMD    "md5sum "
-#define CMD_SIZE   10
-#define RD_ONLY    "r"
-
-#define ERROR_CODE -1
-
-char *output_buffer = NULL;
-
-void exit_program(int exit_status);
+static char *output_buffer = NULL;
 
 int main(void) {
 
+    char command[BUFFER_SIZE + CMD_SIZE];
+
+    FILE *md5_fd;
+    size_t line_buffer = BUFFER_SIZE;
+    char input_buffer[BUFFER_SIZE];
+
+    int nbytes;
+
     while (1) {
-        char input_buffer[BUFFER];
-        char command[BUFFER + CMD_SIZE] = MD5_CMD;
 
-        FILE *md5_fd;
-        size_t line_buffer = BUFFER;
-
-        int nbytes;
-        if ((nbytes = read(0, input_buffer, BUFFER)) == -1) {
-            exit_program(EXIT_FAILURE);
+        if ((nbytes = read(STDIN_FILENO, input_buffer, BUFFER_SIZE)) == ERROR_CODE) {
+            exit_slave(EXIT_FAILURE);
         }
 
         input_buffer[nbytes] = '\0';
@@ -40,28 +31,32 @@ int main(void) {
         strcat(command, input_buffer);
 
         if ((md5_fd = popen(command, RD_ONLY)) == NULL) {
-            perror("popen");
-            exit_program(EXIT_FAILURE);
+            perror("slave | popen");
+            exit_slave(EXIT_FAILURE);
         }
 
-        while (getline((char **)&output_buffer, &line_buffer, md5_fd) != EOF) {
+        while (getline(&output_buffer, &line_buffer, md5_fd) != EOF) {
             printf("process pid: %d | %s", getpid(), output_buffer);
         }
 
-        fflush(stdout);
+        if (fflush(stdout) == EOF){
+            perror("slave | fflush");
+            exit_slave(EXIT_FAILURE);
+        }
 
         if (pclose(md5_fd) == ERROR_CODE) {
-            exit_program(EXIT_FAILURE);
+            perror("slave | pclose");
+            exit_slave(EXIT_FAILURE);
         }
     }
 
-    exit_program(EXIT_SUCCESS);
+    exit_slave(EXIT_SUCCESS);
 }
 
-void exit_program(int exit_status) {
+void exit_slave(int exit_status) {
     if (output_buffer != NULL) {
         free(output_buffer);
     }
 
-    _exit(exit_status);
+    exit(exit_status);
 }
